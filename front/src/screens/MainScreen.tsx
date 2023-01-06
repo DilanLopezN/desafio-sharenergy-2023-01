a6c4ca8c9baf8c7c8e836d41015d953ca46cda81
Dilan Lopez <dilanlopez009@gmail.com>
2023-01-05 23:01:49 -0300

    fix and get api

--- front/src/screens/MainScreen.tsx
+++ front/src/screens/MainScreen.tsx
@@ -1,9 +1,16 @@
 import { Flex } from '@chakra-ui/react'
 import Header from '../components/Header'
 export default function MainScreen() {
+  async function getUser() {
+    const response = await fetch('https://randomuser.me/api')
+    const data = response.json()
+  }
+  getUser()
+
   return (
     <Flex>
       <Header />
+      <div></div>
     </Flex>
   )
 }
